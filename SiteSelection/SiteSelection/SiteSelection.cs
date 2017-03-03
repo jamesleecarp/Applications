fd7c0f2adcf689d861c89440906db412ecd21028
James Carpenter <jamesleecarp@gmail.com>
2017-03-03 09:31:16 -0600

    SiteSelection

---
+++ SiteSelection/SiteSelection/SiteSelection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using ESRI.ArcGIS.ArcMapUI;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.Geometry;
+using System.Windows.Forms;
+
+namespace SiteSelection
+{
+    public class SiteSelection : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public SiteSelection()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            Form1 myForm = new Form1();
+            myForm.ShowDialog();
+        }
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+    }
+
+}
